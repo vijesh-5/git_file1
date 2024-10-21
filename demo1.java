de79292b28bd743f576379da28efe925c43b91d9
vijesh-5 <wvijesh7@gmail.com>
2024-10-21 16:29:57 +0530

    Update demo1.java

--- demo1.java
+++ demo1.java
@@ -3,6 +3,7 @@ package git_file1;
 class demo1{
     public static void main(String [] args){
         System.out.println("hellow world !");
+        System.out.println("hellow fellas ");
     }
 
 }
